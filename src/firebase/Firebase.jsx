2567b52a94bdd40ce2302bfd3976ffd86ae9a395
Aayush Chopra <aayushking37@gmail.com>
2024-02-06 11:58:56 +0530

    changes in env file

--- src/firebase/Firebase.jsx
+++ src/firebase/Firebase.jsx
@@ -3,12 +3,12 @@ import { GoogleAuthProvider, getAuth } from "firebase/auth"
 import { getFirestore, collection } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBYIDY4FS6xXN7eO__bepBh44-Qu9ITz9Q",
-  authDomain: "personal-blogs-a3bbb.firebaseapp.com",
-  projectId: "personal-blogs-a3bbb",
-  storageBucket: "personal-blogs-a3bbb.appspot.com",
-  messagingSenderId: "127806095418",
-  appId: "1:127806095418:web:c843ce0831e15f6979f503"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
